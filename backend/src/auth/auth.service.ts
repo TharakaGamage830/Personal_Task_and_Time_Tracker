9842b0217358ef1e279d30a3932802051f0c0189
TharakaGamage830 <tharakaashen830@gmail.com>
2026-02-08 00:05:38 +0530

    chore(backend): add auth.service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) { }
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.usersService.findByEmail(email);
+        if (!user) {
+            return null;
+        }
+        if (await bcrypt.compare(pass, user.password_hash)) {
+            const { password_hash, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    async login(user: any) {
+        const payload = { email: user.email, sub: user.id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+
+    async register(email: string, passwrd: string, name: string){
+        const existingUser = await this.usersService.findByEmail(email);
+        if (existingUser) {
+            throw new UnauthorizedException('User already exists');
+        }
+
+        const salt = await bcrypt.genSalt();
+        const password_hash = await bcrypt.hash(passwrd, salt);
+        return this.usersService.create({ email, password_hash, name });
+    }
+
+}
