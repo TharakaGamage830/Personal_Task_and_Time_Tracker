742d3e5f8cbcea03f6a62c8e7e0951952afb434f
TharakaGamage830 <tharakaashen830@gmail.com>
2026-02-08 00:10:39 +0530

    chore(backend): add auth.controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('login')
+    async login(@Body() body){
+        const user = await this.authService.validateUser(body.email, body.password);
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        return this.authService.login(user);
+    }
+
+    @Post('register')
+    async register(@Body() body) {
+        return this.authService.register(body.email, body.password, body.name);
+    }
+
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('me')
+    getProfile(@Request() req) {
+        return req.user;
+    }
+    
+}
