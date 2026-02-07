3b546a169cf1e1f28ec1c060f15d99a547084b3a
TharakaGamage830 <tharakaashen830@gmail.com>
2026-02-08 00:59:37 +0530

    Refactor(backend): Modefied time tracking module and finished

--- backend/src/time-tracking/time-tracking.controller.ts
+++ backend/src/time-tracking/time-tracking.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { TimeTrackingService } from './time-tracking.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('time-tracking')
-export class TimeTrackingController {}
+export class TimeTrackingController {
+
+    constructor(private readonly timeTrackingService: TimeTrackingService) { }
+
+    @Post(':id/timer/start')
+    start(@Param('id') id: string, @Request() req) {
+        return this.timeTrackingService.startTimer(+id, req.user.userId);
+    }
+
+    @Post(':id/timer/stop')
+    stop(@Param('id') id: string, @Request() req) {
+        return this.timeTrackingService.stopTimer(+id, req.user.userId);
+    }
+
+    @Get(':id/sessions')
+    getSessions(@Param('id') id: string, @Request() req) {
+        return this.timeTrackingService.getSessions(+id, req.user.userId);
+    }
+
+
+}
--- backend/src/time-tracking/time-tracking.service.ts
+++ backend/src/time-tracking/time-tracking.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TasksService } from 'src/tasks/tasks.service';
+import { IsNull, Repository } from 'typeorm';
+import { TimeSession } from './time-session.entity';
 
 @Injectable()
-export class TimeTrackingService {}
+export class TimeTrackingService {
+
+    constructor(
+        @InjectRepository(TimeSession)
+        private sessionsRepository: Repository<TimeSession>,
+        private tasksService: TasksService,
+    ) { }
+
+    async startTimer(taskId: number, userId: number): Promise<TimeSession> {
+        const task = await this.tasksService.findOne(taskId, userId);
+        const runningSession = await this.sessionsRepository.findOne({
+            where: { task_id: taskId, end_time: IsNull() },
+        });
+
+        if (runningSession) {
+            throw new BadRequestException('Timer already running for this task');
+        }
+         const session = this.sessionsRepository.create({
+            task_id: taskId,
+            start_time: new Date(),
+        });
+        return this.sessionsRepository.save(session);
+
+    }
+
+    async stopTimer(taskId: number, userId: number): Promise<TimeSession> {
+        const task = await this.tasksService.findOne(taskId, userId);
+
+        const runningSession = await this.sessionsRepository.findOne({
+            where: { task_id: taskId, end_time: IsNull() },
+        });
+
+        if (!runningSession) {
+            throw new BadRequestException('No running timer for this task');
+        }
+
+        runningSession.end_time = new Date();
+        const durationMs = runningSession.end_time.getTime() - runningSession.start_time.getTime();
+        runningSession.duration_seconds = Math.floor(durationMs / 1000);
+
+        await this.sessionsRepository.save(runningSession);
+
+        task.total_time_seconds += runningSession.duration_seconds;
+        await this.tasksService.update(taskId, { total_time_seconds: task.total_time_seconds }, userId);
+
+        return runningSession;
+    }
+
+    async getSessions(taskId: number, userId: number): Promise<TimeSession[]> {
+        await this.tasksService.findOne(taskId, userId);
+        return this.sessionsRepository.find({ where: { task_id: taskId }, order: { start_time: 'DESC' } });
+    }
+
+}
