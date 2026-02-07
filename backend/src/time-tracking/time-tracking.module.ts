5eb4e9878f71ef7688843e771f62f37bab2fceed
TharakaGamage830 <tharakaashen830@gmail.com>
2026-02-07 23:53:22 +0530

    chore(backend): add user services

--- backend/src/time-tracking/time-tracking.module.ts
+++ backend/src/time-tracking/time-tracking.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TimeTrackingService } from './time-tracking.service';
 import { TimeTrackingController } from './time-tracking.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from 'src/tasks/tasks.module';
+import { TimeSession } from './time-session.entity';
 
 @Module({
+  imports: [
+        TypeOrmModule.forFeature([TimeSession]),
+        TasksModule,
+    ],
   providers: [TimeTrackingService],
-  controllers: [TimeTrackingController]
+  controllers: [TimeTrackingController],
+  exports: [TimeTrackingService],
 })
 export class TimeTrackingModule {}
